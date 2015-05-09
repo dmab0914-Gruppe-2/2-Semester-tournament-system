3d8ae1f2491af3d6c53d6b29814e0fe3b8aa121e
Q11x <q11x@q11x.net>
2015-05-09 23:21:47 +0200

    Created unused EliminationController

---
+++ src/CtrLayer/EliminationController.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package CtrLayer;
+
+/**
+ * @author Jacob
+ *
+ */
+public class EliminationController {
+
+	/**
+	 * 
+	 */
+	public EliminationController() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
