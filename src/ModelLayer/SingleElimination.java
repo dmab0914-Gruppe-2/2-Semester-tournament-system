bd5c1b0be0426428bf1ccf6640603836d8d92d9a
Splint <splint@splintnet.dk>
2015-05-11 09:36:32 +0200

    Quick draft to idea of how to generate a list of matches

--- src/ModelLayer/SingleElimination.java
+++ src/ModelLayer/SingleElimination.java
@@ -10,4 +10,17 @@ public class SingleElimination {
     public SingleElimination(ArrayList<Team> teams) {
 
     }//endConstructor
+
+    public ArrayList<Match> generateSETournament(ArrayList<Team> teams) {
+
+        if (teams.size() % 2 == 0) {
+            System.out.println("Teams are divisible by two" + teams.size());
+
+        } else if (teams.size() % 2 != 0) {
+            System.out.println("Teams are NOT divisible by two" + teams.size());
+        }
+
+
+        return new ArrayList<Match>();
+    }//end generateSETournament
 }//endClass
