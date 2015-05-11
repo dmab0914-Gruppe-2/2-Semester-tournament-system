4805a9bf441d8638ef318a0a75281072e374546f
Splint <splint@splintnet.dk>
2015-05-11 09:35:29 +0200

    New Imports Fields for team, Tournament and SingleElimination public methods for generating Single elimination tournaments and for retrieving participating teams in a match

--- src/CtrLayer/EliminatorController.java
+++ src/CtrLayer/EliminatorController.java
@@ -1,22 +1,42 @@
 package CtrLayer;
 
+import DBLayer.DBConnection;
+import ModelLayer.Match;
+import ModelLayer.SingleElimination;
 import ModelLayer.Team;
+import ModelLayer.Tournament;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
  * Created by Andreas on 09-05-2015.
  */
 public class EliminatorController {
+    private ArrayList<Team> teams;
+    private Tournament t;
+    private SingleElimination singleElimination;
 
 
     public EliminatorController() {
+        generateDummyTeams();
     }//endConstructor
 
-    public Team generateSETournament(ArrayList<Team> teams){
-        return new Team();
+    public void generateDummyTeams() {
 
+    }
+
+    public ArrayList<Match> generateSETournament()
+    {
+        return singleElimination.generateSETournament(teams);
+    }
+
+    public Tournament generateDETournament(ArrayList<Team> teams) {
+        return new Tournament();
+    }
+
+    public ArrayList<Team> getTeams(int tournamentId)
+    {
+        return t.getTeams();
     }
 
 }//endClass
