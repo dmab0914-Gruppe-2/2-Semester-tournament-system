c1e332bdd400f6f484290eb9abe3dc059296b213
Splint <splint@splintnet.dk>
2015-05-11 09:33:11 +0200

    New Match Class, no code

---
+++ src/ModelLayer/Match.java
@@ -0,0 +1,14 @@
+package ModelLayer;
+
+/**
+ * Created by Andreas on 11-05-2015.
+ */
+public class Match {
+    Team team1;
+    Team team2;
+
+    public Match(Team team1, Team team2) {
+        this.team1 = team1;
+        this.team2 = team2;
+    }
+}
