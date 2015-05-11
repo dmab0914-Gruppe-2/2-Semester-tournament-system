5576b6b9e17618cf4cc145f1e18d6d5b07142494
SlyCooper93 <ronnie93@live.dk>
2015-05-11 11:33:02 +0200

    Team
    array og set og get

--- src/ModelLayer/Team.java
+++ src/ModelLayer/Team.java
@@ -3,15 +3,31 @@ package ModelLayer;
 import java.util.ArrayList;
 
 /**
- * Created by Andreas on 10-05-2015.
+ * Created by Ronnie on 10-05-2015.
  */
 public class Team {
 
     private String name;
-    private ArrayList<Player> players;
+    private ArrayList<User> users;
 
-    public Team(String name, ArrayList<Player> players) {
-        this.name = name;
-        this.players = players;
+    public Team(String name, ArrayList<User> users) {
+        this.setName(name);
+        this.setUsers(users);
     }
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public ArrayList<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(ArrayList<User> users) {
+		this.users = users;
+	}
 }
