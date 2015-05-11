66bf814b6e5a664fa76fc5d51204a2adf2c496bb
welle93 <welle93@gmail.com>
2015-05-11 11:31:52 +0200

    User modellayer added
    User modellayer added
    with get / set

--- src/ModelLayer/User.java
+++ src/ModelLayer/User.java
@@ -8,5 +8,84 @@ package ModelLayer;
  *
  */
 public class User {
+	private int userID;
+	private String handle;
+	private String name;
+	private Team team;
+
+	/**
+	 * Constructor for a all fields set!
+	 */
+	
+	public User(int id, String handle, String name, Team team) {
+		this.userID = id;
+		this.handle = handle;
+		this.name = name;
+		this.team = team;
+	}
+	
+	/**
+	 * Constructor for a empty User
+	 */
+	
+	public User() {
+
+	}
+
+	/**
+	 * @return the userID
+	 */
+	public int getUserID() {
+		return userID;
+	}
+
+	/**
+	 * @param userID the userID to set
+	 */
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+	/**
+	 * @return the handle
+	 */
+	public String getHandle() {
+		return handle;
+	}
+
+	/**
+	 * @param handle the handle to set
+	 */
+	public void setHandle(String handle) {
+		this.handle = handle;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the team
+	 */
+	public String getTeam() {
+		return team;
+	}
+
+	/**
+	 * @param team the team to set
+	 */
+	public void setTeam(String team) {
+		this.team = team;
+	}
 
 }
