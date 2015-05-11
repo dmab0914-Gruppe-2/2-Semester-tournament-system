57e993e96ddc7bec1260eea89280189a70c6cc18
Splint <splint@splintnet.dk>
2015-05-11 09:37:29 +0200

    Added Arraylist Import Added private fields Added basic constructor

--- src/ModelLayer/Team.java
+++ src/ModelLayer/Team.java
@@ -1,7 +1,17 @@
 package ModelLayer;
 
+import java.util.ArrayList;
+
 /**
  * Created by Andreas on 10-05-2015.
  */
 public class Team {
+
+    private String name;
+    private ArrayList<Player> players;
+
+    public Team(String name, ArrayList<Player> players) {
+        this.name = name;
+        this.players = players;
+    }
 }
