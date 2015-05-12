42ed9c5a72a2e77db9e8c26227d101e32f3f75d8
Splint <splint@splintnet.dk>
2015-05-12 13:36:12 +0200

    Small changes to dynamic matches Added matches based on scores Added generateMatch method Added getMaxScore method Renamed to method to generateSERound

--- src/ModelLayer/SingleElimination.java
+++ src/ModelLayer/SingleElimination.java
@@ -13,63 +13,154 @@ public class SingleElimination {
 
     }//endConstructor
 
-    public ArrayList<Match> generateSETournament(ArrayList<Team> teams, ArrayList<Integer> scores) {
+    public ArrayList<Match> generateSERound(ArrayList<Team> teams, ArrayList<Integer> scores) {
 
         ArrayList<Match> matches = new ArrayList<>();
         if (scores.size() == 0) {
 
-            if (teams.size() % 2 == 0) {
-                System.out.println("Teams are divisible by two" + teams.size());
+            if (teams.size() % 2 == 0 || teams.size() == 2) {
+                System.out.println("Teams are divisible by two, number of teams: " + teams.size());
 
-                for (int i = 0; i < teams.size(); i += 2) {
-                    Random rnd = new Random();
+                while(teams.size() > 0) {
 
-                    Match match = new Match();
+
                     int rndNumber = rndNumber(teams.size());
                     Team team1 = teams.get(rndNumber);
-                    match.setTeam1(team1);
+
                     teams.remove(team1);
 
                     rndNumber = rndNumber(teams.size());
                     Team team2 = teams.get(rndNumber);
-                    match.setTeam2(team2);
+
+                    teams.remove(team2);
+                    Match match = new Match();
+                    generateMatch(team1, team2);
                     matches.add(match);
+
+                    System.out.printf("Remaining teams: " + teams.size() +"\n");
 
                 }//endFor
 
             } else if (teams.size() % 2 != 0) {
 
-                System.out.println("Teams are NOT divisible by two" + teams.size());
+                System.out.println("Teams are NOT divisible by two, number of teams: " + teams.size());
 
-                for (int i = 0; i < teams.size(); i += 2) {
+                while (teams.size() > 0){
 
-                    if(teams.size() - i <= 3){
+                    if(teams.size() >= 3){
                         System.out.println("Teams - Index <= 3 ");
-                        Random rnd = new Random();
 
-                        Match match = new Match();
-                        Team team1 = teams.get(rnd.nextInt(i));
-                        match.setTeam1(team1);
+                        int rndNumber = rndNumber(teams.size());
+                        Team team1 = teams.get(rndNumber);
                         teams.remove(team1);
 
-                        Team team2 = teams.get(rnd.nextInt(i - 1));
-                        match.setTeam2(team2);
+                        rndNumber = rndNumber(teams.size());
+                        Team team2 = teams.get(rndNumber);
+                        teams.remove(team2);
+                        Match match = generateMatch(team1, team2);
                         matches.add(match);
-                    }else if(teams.size() - i == 1){
+
+                    }else if(teams.size() >= 1){
                         System.out.println("Teams - Index == 1 ");
-
+                        Match match = new Match();
+                        Team team = teams.get(0);
+                        match.setTeam1(team);
+                        teams.remove(team);
                     }//endIf
+                    System.out.printf("Number of teams left: "+teams.size() + "\n");
                 }//endFor
+            }//endIf
 
+        } else if(scores.size() != 0){
+            if(teams.size() == scores.size()){
+
+                boolean complete = false;
+
+                //generates matches
+                while (!complete) {
+
+                    int maxScorePos = getMaxScore(scores);
+
+
+                    if(maxScorePos != -1){
+                        Team team1 = teams.get(maxScorePos);
+                        Team team2;
+                        scores.remove(maxScorePos);
+                        teams.remove(maxScorePos);
+                        maxScorePos = getMaxScore(scores);
+
+                        if(maxScorePos != -1){
+                            team2 = teams.get(maxScorePos);
+                            scores.remove(maxScorePos);
+                            teams.remove(maxScorePos);
+                        } else {
+                            throw new IndexOutOfBoundsException();
+                        }//endIf
+
+                        Match match = generateMatch(team1, team2);
+                        matches.add(match);
+
+
+                    } else {
+                        throw new IndexOutOfBoundsException();
+                    }//endIf
+
+
+
+                    if(scores.size() == 0 && teams.size() == 0){
+                        complete = true;
+                    }//endIf
+
+                }//endWhile
+
+            } else {
+                throw new WrongDataInputException(
+                        "Amount of scores does NOT correlate with the amount of teams!" +
+                        "\nScores : " + scores.size() +
+                        "\nTeams  : " + teams.size() + "\n");
             }
         }//endIf
         return matches;
     }//end generateSETournament
 
+    /**
+     * Designed to generate a random number between 0 and maximum.
+     * @param input Maximum
+     * @return random Integer between0 and input
+     */
     public int rndNumber(int input){
         Random rnd = new Random();
         int i  = rnd.nextInt(input);
-        //System.out.println("Uber Random Number " + i);
         return i;
     }//endRndNumber
+
+    /**
+     * Generates a match of two teams
+     * @param one team one participating in the tournament
+     * @param two team two participating in the tournament
+     * @return match with team one and team two battling against each other
+     */
+    public Match generateMatch(Team one, Team two){
+        Match match = new Match();
+        match.setTeam1(one);
+        match.setTeam2(two);
+        match.setStatus(Match.Status.waiting);
+        return match;
+    }
+
+    public int getMaxScore(ArrayList<Integer> scores){
+        boolean max = false;
+        int i = 0;
+        int maxValue = -1;
+        int maxPos = -1;
+        while (!max){
+            if(scores.get(i) > maxValue){
+                maxPos = i;
+                maxValue = scores.get(i);
+
+            }//endIf
+            i++;
+        }//endWhile
+        return maxPos;
+    }//endMethod
 }//endClass
