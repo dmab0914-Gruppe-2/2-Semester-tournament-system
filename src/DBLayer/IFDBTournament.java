aceac00208bece8d481dd457c9cf0d2c6f6bbe4c
Q11x <q11x@q11x.net>
2015-05-12 13:42:10 +0200

    Created interface for DBTournament

---
+++ src/DBLayer/IFDBTournament.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package DBLayer;
+
+import java.util.ArrayList;
+
+import ModelLayer.Team;
+import ModelLayer.Tournament;
+
+/**
+ * @author Jacob 12/05/2015
+ *
+ */
+public interface IFDBTournament {
+	
+	public ArrayList<Tournament> getTournaments();
+	
+	public Tournament getTournament(int tournamentID);
+	
+	public boolean enableSignup(int tournamentID);
+	
+	public boolean startTournament(int tournamentID);
+	
+	public ArrayList<Team> getTournamentTeams(int tournamentID);
+	
+	public int advanceTournament(int tournamentID);
+	
+	public Tournament endTournament(int tournamentID);
+}
