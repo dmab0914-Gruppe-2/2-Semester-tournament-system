6ec9cd3e510c7f19abb250b6a7121cda3c2f88a2
Splint <splint@splintnet.dk>
2015-05-12 08:33:00 +0200

    Optimized import Added generateDummyTeams method for testing Added untested generateSETournament

--- src/CtrLayer/EliminatorController.java
+++ src/CtrLayer/EliminatorController.java
@@ -1,10 +1,6 @@
 package CtrLayer;
 
-import DBLayer.DBConnection;
-import ModelLayer.Match;
-import ModelLayer.SingleElimination;
-import ModelLayer.Team;
-import ModelLayer.Tournament;
+import ModelLayer.*;
 
 import java.util.ArrayList;
 
@@ -12,31 +8,44 @@ import java.util.ArrayList;
  * Created by Andreas on 09-05-2015.
  */
 public class EliminatorController {
-    private ArrayList<Team> teams;
-    private Tournament t;
     private SingleElimination singleElimination;
 
 
     public EliminatorController() {
-        generateDummyTeams();
+        singleElimination = new SingleElimination();
+
+        //generateSETournament(generateDummyTeams(), new ArrayList<Integer>());
     }//endConstructor
 
-    public void generateDummyTeams() {
-
+    public ArrayList<Team> generateDummyTeams() {
+        int i= 0;
+        ArrayList<Team> teams = new ArrayList<>();
+        while(i<10){
+            String s = "The Awesome Thingy No." + Integer.toString(i);
+            Team team = new Team(s, new ArrayList<Player>());
+            System.out.println("Team name: " + s);
+            teams.add(team);
+            i++;
+        }
+        return teams;
     }
 
-    public ArrayList<Match> generateSETournament()
+    /**
+     *
+     * @param teams A list of participating teams in the set of matches
+     * @param scores A list of scores from last Round which is being used to generate next round with set scores.
+     *               If this is the first match, throw an empty list of scores and a random setup will be generated
+     * @return ArrayList of Match(es) to be played
+     */
+    public ArrayList<Match> generateSETournament(ArrayList<Team> teams, ArrayList<Integer> scores)
     {
-        return singleElimination.generateSETournament(teams);
-    }
-
-    public Tournament generateDETournament(ArrayList<Team> teams) {
-        return new Tournament();
-    }
-
-    public ArrayList<Team> getTeams(int tournamentId)
-    {
-        return t.getTeams();
+        System.out.println("Match lineup: ");
+        ArrayList<Match> matches = singleElimination.generateSETournament(teams, scores);
+        for(int i = 0; i<matches.size(); i++){
+            System.out.println(matches.get(i).getTeam1().getName());
+            System.out.println(matches.get(i).getTeam2().getName());
+        }
+        return matches;
     }
 
 }//endClass
