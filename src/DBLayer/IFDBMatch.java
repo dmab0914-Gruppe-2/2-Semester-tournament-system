21101c440175d794e98bbb462772a8bba2cc4dfc
Q11x <q11x@q11x.net>
2015-05-12 12:55:13 +0200

    Created interface for DBMatch

---
+++ src/DBLayer/IFDBMatch.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package DBLayer;
+
+import java.util.ArrayList;
+
+import ModelLayer.Match;
+
+/**
+ * @author Jacob 12/05/2015
+ *
+ */
+public interface IFDBMatch {
+	
+	public Match addmatch(Match match);
+	
+	public boolean setMatchResults(int matchID, int team1Score, int team2Score);
+	
+	public ArrayList<Match> getMatches(int tournamentID);
+}
