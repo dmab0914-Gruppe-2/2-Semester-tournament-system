fd647e2d4ccaaf9e990cd2e017f9831c9469c39c
Splint <splint@splintnet.dk>
2015-05-12 08:33:22 +0200

    Added Match with getters and setters

--- src/ModelLayer/Match.java
+++ src/ModelLayer/Match.java
@@ -4,11 +4,50 @@ package ModelLayer;
  * Created by Andreas on 11-05-2015.
  */
 public class Match {
-    Team team1;
-    Team team2;
 
-    public Match(Team team1, Team team2) {
+    private Team team1;
+    private Team team2;
+    private int team1Score;
+    private int team2Score;
+
+    public Match() {
+
+    }
+
+
+    public Team getTeam1() {
+        return team1;
+    }
+
+    public void setTeam1(Team team1) {
         this.team1 = team1;
+    }
+
+    public Team getTeam2() {
+        return team2;
+    }
+
+    public void setTeam2(Team team2) {
         this.team2 = team2;
     }
+
+    public int getTeam1Score() {
+        return team1Score;
+    }
+
+    public void setTeam1Score(int team1Score) {
+        this.team1Score = team1Score;
+    }
+
+    public int getTeam2Score() {
+        return team2Score;
+    }
+
+    public void setTeam2Score(int team2Score) {
+        this.team2Score = team2Score;
+    }
+
+
+
+
 }
