ad5b95560a83a3fe3283f426587c59e265d8b74e
Splint <splint@splintnet.dk>
2015-05-13 12:17:03 +0200

    Added empty testclass for EliminationController with JUnit4

---
+++ src/CtrLayer/TestEliminationController.java
@@ -0,0 +1,28 @@
+package CtrLayer;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Andreas on 13-05-2015.
+ */
+public class TestEliminationController {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGenerateSERound() throws Exception {
+
+    }
+}
