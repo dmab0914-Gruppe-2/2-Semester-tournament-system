12d60590d59f299856e4f5de31d0987920409eb6
Q11x <q11x@q11x.net>
2015-05-13 11:55:18 +0200

    Removed test method from interface

--- src/CtrLayer/IFEliminationController.java
+++ src/CtrLayer/IFEliminationController.java
@@ -8,12 +8,6 @@ import ModelLayer.Team;
 public interface IFEliminationController {
 
 	/**
-	 * Generates dummy classes for testing purposes
-	 * @return list of dummy classes
-	 */
-	public abstract ArrayList<Team> generateDummyTeams();//endMethod
-
-	/**
 	 *
 	 * @param teams A list of participating teams in the set of matches
 	 * @param scores A list of scores from last Round which is being used to generate next round with set scores.
