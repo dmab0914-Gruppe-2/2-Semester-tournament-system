5f8e33d6f5c196a05e6d75454c0ba438940541dc
Q11x <q11x@q11x.net>
2015-05-13 13:57:07 +0200

    added method to remove a Match from db

--- src/DBLayer/IFDBMatch.java
+++ src/DBLayer/IFDBMatch.java
@@ -15,6 +15,8 @@ public interface IFDBMatch {
 	
 	public Match addmatch(Match match);
 	
+	public Match removeMatch(int matchID);
+	
 	public boolean setMatchResults(int matchID, int team1Score, int team2Score);
 	
 	public ArrayList<Match> getMatches(int tournamentID);
