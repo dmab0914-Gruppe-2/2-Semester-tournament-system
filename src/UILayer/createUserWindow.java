fda8ea241807069f8a77455c9e1a04334add1d1c
Nicklas Welle <welle93@gmail.com>
2015-05-20 12:33:07 +0200

    Create user window done
    So much rage used for this!

--- src/UILayer/createUserWindow.java
+++ src/UILayer/createUserWindow.java
@@ -1,4 +1,5 @@
 package UILayer;
+
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 
@@ -7,17 +8,39 @@ import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import java.awt.CardLayout;
 
-public class createUserWindow extends JDialog {
+import javax.swing.BoxLayout;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JLabel;
+
+import java.awt.Font;
+
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
+
+import CtrLayer.UserController;
+
+public class CreateUserWindow extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
+	private JTextField txtHandle;
+	private JTextField txtName;
+	private JTextField txtPassword;
+	private UserController uC;
+	private JLabel lblStatus;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void createUserWindow() {
 		try {
-			createUserWindow dialog = new createUserWindow();
+
+			CreateUserWindow dialog = new CreateUserWindow();
 			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 			dialog.setVisible(true);
 		} catch (Exception e) {
@@ -28,28 +51,110 @@ public class createUserWindow extends JDialog {
 	/**
 	 * Create the dialog.
 	 */
-	public createUserWindow() {
-		setBounds(100, 100, 450, 300);
-		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
+	public CreateUserWindow() {
+
+		setBounds(100, 100, 425, 370);
+		getContentPane().setLayout(null);
+		contentPanel.setBounds(0, 0, 409, 293);
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		getContentPane().add(contentPanel);
+		contentPanel.setLayout(null);
+		{
+			JLabel lblNewLabel = new JLabel("Create new user");
+			lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
+			lblNewLabel.setBounds(10, 11, 136, 14);
+			contentPanel.add(lblNewLabel);
+		}
+		{
+			JSeparator separator = new JSeparator();
+			separator.setBounds(10, 36, 599, 2);
+			contentPanel.add(separator);
+		}
+		{
+			JLabel lblHandlealias = new JLabel("Handle (alias):");
+			lblHandlealias.setFont(new Font("Tahoma", Font.PLAIN, 14));
+			lblHandlealias.setBounds(54, 66, 92, 14);
+			contentPanel.add(lblHandlealias);
+		}
+
+		txtHandle = new JTextField();
+		txtHandle.setBounds(248, 65, 120, 20);
+		contentPanel.add(txtHandle);
+		txtHandle.setColumns(10);
+
+		JLabel lblName = new JLabel("Name:");
+		lblName.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblName.setBounds(54, 146, 92, 14);
+		contentPanel.add(lblName);
+
+		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblPassword.setBounds(54, 226, 92, 14);
+		contentPanel.add(lblPassword);
+		{
+			txtName = new JTextField();
+			txtName.setBounds(248, 144, 120, 20);
+			contentPanel.add(txtName);
+			txtName.setColumns(10);
+		}
+		{
+			txtPassword = new JTextField();
+			txtPassword.setBounds(248, 223, 120, 20);
+			contentPanel.add(txtPassword);
+			txtPassword.setColumns(10);
+		}
+		{
+			lblStatus = new JLabel("");
+			lblStatus.setBounds(152, 268, 105, 14);
+			contentPanel.add(lblStatus);
+		}
 		{
 			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			buttonPane.setBounds(0, 304, 409, 27);
+			getContentPane().add(buttonPane);
+			buttonPane.setLayout(new GridLayout(0, 2, 0, 0));
 			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
+				JButton btnCreate = new JButton("Create");
+				btnCreate.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						createUser();
+					}
+				});
+				btnCreate.setActionCommand("OK");
+				buttonPane.add(btnCreate);
+				getRootPane().setDefaultButton(btnCreate);
 			}
 			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
+				JButton btnClose = new JButton("Close / Cancel");
+				btnClose.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dispose();
+					}
+				});
+				btnClose.setActionCommand("Cancel");
+				buttonPane.add(btnClose);
 			}
 		}
 	}
 
+	private void createUser() {
+		UserController userCtr = new UserController();
+		if (userCtr.findUserByHandle(txtHandle.getText()) == null) {
+			if (txtPassword.getText().length() >= 3
+					&& txtName.getText().length() > 2) {
+				try {
+					userCtr.addUser(txtHandle.getText(), txtName.getText(),
+							txtPassword.getText(), false);
+					lblStatus.setText("User Created");
+					txtHandle.setText("");
+					txtName.setText("");
+					txtPassword.setText("");
+				} catch (Exception e) {
+					lblStatus.setText("Error in inputs, please try again!");
+				}
+			} else
+				lblStatus.setText("Please, fill out all fields Thanks :)");
+		}else
+			lblStatus.setText("Sorry this handle is taken :(");
+	}
 }
