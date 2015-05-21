afcc72591f74ef14d5bd60cb6fe87553de1e302c
Splint <splint@splintnet.dk>
2015-05-21 10:37:41 +0200

    moved commented test to UserController which is in effect

--- src/Main.java
+++ src/Main.java
@@ -24,21 +24,5 @@ public class Main {
 			e.printStackTrace();
 		}
 	}
-	
-	/*private void andreasTest() throws Exception {
-		System.out.print("Please enter a password for hashing: ");
-        String password = System.console().readLine();
 
-        MessageDigest messageDigest = MessageDigest.getInstance("SHA-512");
-        messageDigest.update(password.getBytes());
-
-        byte byteData[] = messageDigest.digest();
-
-        //Convert to HEX 1
-        StringBuffer stringBuffer = new StringBuffer();
-        for(int i = 0; i<byteData.length; i++){
-            stringBuffer.append(Integer.toString(byteData[i] & 0xff + 0x100, 16).substring(1));
-        }
-        System.out.print("HEX Format method 1: "+stringBuffer.toString() + "\n");
-	}*/
 }
