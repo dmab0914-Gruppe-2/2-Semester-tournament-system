253d8cc6a87d3a6ff8f9c5f13b605f2ec2a1c37e
Splint <splint@splintnet.dk>
2015-05-21 10:35:21 +0200

    General bugfixing and cleanup

--- src/CtrLayer/EliminationController.java
+++ src/CtrLayer/EliminationController.java
@@ -18,23 +18,6 @@ public class EliminationController implements IFEliminationController {
     }//endConstructor
 
     /**
-     * Generates dummy classes for testing purposes
-     * @return list of dummy classes
-     */
-    public ArrayList<Team> generateDummyTeams() {
-        int i= 0;
-        ArrayList<Team> teams = new ArrayList<>();
-        while(i<11){
-            String s = "The Awesome Thingy No." + Integer.toString(i);
-            Team team = new Team(s, new ArrayList<User>());
-            System.out.println("Team name: " + s);
-            teams.add(team);
-            i++;
-        }
-        return teams;
-    }//endMethod
-
-    /**
      *
      * @param teams A list of participating teams in the set of matches
      * @param scores A list of scores from last Round which is being used to generate next round with set scores.
@@ -44,9 +27,77 @@ public class EliminationController implements IFEliminationController {
     public ArrayList<Match> generateRound(ArrayList<Team> teams, ArrayList<Integer> scores) {
 
         ArrayList<Match> matches = new ArrayList<>();
-        if (scores.size() == 0) {
 
-            if (teams.size() % 2 == 0 || teams.size() == 2) {
+        /**
+         * Scores and teams are the same size
+         */
+        if(teams.size() == scores.size()){
+
+            boolean complete = false;
+
+            //generates matches
+            while (!complete) {
+
+                int maxScorePos = getMaxScore(scores);
+
+
+                if (maxScorePos != -1) {
+
+                    Team team1 = teams.get(maxScorePos);
+                    teams.remove(maxScorePos);
+                    scores.remove(maxScorePos);
+                    Team team2;
+
+                    if(teams.size() >= 1){
+                        maxScorePos = getMaxScore(scores);
+                        team2 = teams.get(maxScorePos);
+                        scores.remove(maxScorePos);
+                        teams.remove(maxScorePos);
+                    }else{ // if (teams.size() >= 1){
+                        team2 = new Team();
+                    }//else {
+                        //throw new IndexOutOfBoundsException();
+                    //}
+
+
+
+
+                    /*if (teams.size() >= 2) {
+
+                        maxScorePos = getMaxScore(scores);
+                        if (maxScorePos != -1) {
+                            team2 = teams.get(maxScorePos);
+                            scores.remove(maxScorePos);
+                            teams.remove(maxScorePos);
+                        } else {
+                            throw new IndexOutOfBoundsException();
+                        }//endIf
+                    } else {
+                        team2 = new Team();
+                    }*/
+
+                    Match match = generateMatch(team1, team2);
+                    matches.add(match);
+
+                } else {
+                    throw new IndexOutOfBoundsException();
+                }//endIf
+
+
+                if (scores.size() == 0 && teams.size() == 0) {
+                    complete = true;
+                }else if(scores.size() != teams.size()){
+                    throw new IndexOutOfBoundsException();
+                }//endIf
+
+            }//endWhile
+
+
+        /**
+         * No scores given, random generation
+         */
+        } else if(scores.size() == 0 && teams.size() > 1){
+            if(teams.size() % 2 == 0 || teams.size() == 2){
                 System.out.println("Teams are divisible by two, number of teams: " + teams.size());
 
                 while(teams.size() > 0) {
@@ -65,88 +116,44 @@ public class EliminationController implements IFEliminationController {
                     generateMatch(team1, team2);
                     matches.add(match);
 
-                    System.out.printf("Remaining teams: " + teams.size() +"\n");
+                    //System.out.printf("Remaining teams: " + teams.size() +"\n");
 
                 }//endFor
-
             } else if (teams.size() % 2 != 0) {
 
-                System.out.println("Teams are NOT divisible by two, number of teams: " + teams.size());
+            System.out.println("Teams are NOT divisible by two, number of teams: " + teams.size());
 
-                while (teams.size() > 0){
+            while (teams.size() > 0){
 
-                    if(teams.size() >= 3){
-                        System.out.println("Teams - Index <= 3 ");
+                if(teams.size() >= 3){
+                    int rndNumber = rndNumber(teams.size());
+                    Team team1 = teams.get(rndNumber);
+                    teams.remove(team1);
 
-                        int rndNumber = rndNumber(teams.size());
-                        Team team1 = teams.get(rndNumber);
-                        teams.remove(team1);
+                    rndNumber = rndNumber(teams.size());
+                    Team team2 = teams.get(rndNumber);
+                    teams.remove(team2);
+                    Match match = generateMatch(team1, team2);
+                    matches.add(match);
 
-                        rndNumber = rndNumber(teams.size());
-                        Team team2 = teams.get(rndNumber);
-                        teams.remove(team2);
-                        Match match = generateMatch(team1, team2);
-                        matches.add(match);
+                }else if(teams.size() == 1){
+                    Match match = new Match();
+                    Team team = teams.get(0);
+                    match.setTeam1(team);
+                    matches.add(match);
+                    teams.remove(team);
+                }//endIf
+            }//endFor
+        }//endIf
 
-                    }else if(teams.size() >= 1){
-                        System.out.println("Teams - Index == 1 ");
-                        Match match = new Match();
-                        Team team = teams.get(0);
-                        match.setTeam1(team);
-                        teams.remove(team);
-                    }//endIf
-                    System.out.printf("Number of teams left: "+teams.size() + "\n");
-                }//endFor
-            }//endIf
-
-        } else if(scores.size() != 0){
-            if(teams.size() == scores.size()){
-
-                boolean complete = false;
-
-                //generates matches
-                while (!complete) {
-
-                    int maxScorePos = getMaxScore(scores);
-
-
-                    if(maxScorePos != -1){
-                        Team team1 = teams.get(maxScorePos);
-                        Team team2;
-                        scores.remove(maxScorePos);
-                        teams.remove(maxScorePos);
-                        maxScorePos = getMaxScore(scores);
-
-                        if(maxScorePos != -1){
-                            team2 = teams.get(maxScorePos);
-                            scores.remove(maxScorePos);
-                            teams.remove(maxScorePos);
-                        } else {
-                            throw new IndexOutOfBoundsException();
-                        }//endIf
-
-                        Match match = generateMatch(team1, team2);
-                        matches.add(match);
-
-
-                    } else {
-                        throw new IndexOutOfBoundsException();
-                    }//endIf
-
-
-
-                    if(scores.size() == 0 && teams.size() == 0){
-                        complete = true;
-                    }//endIf
-
-                }//endWhile
-
-            } else {
-                throw new WrongDataInputException(
-                        "Amount of scores does NOT correlate with the amount of teams!" +
-                                "\nScores : " + scores.size() +
-                                "\nTeams  : " + teams.size() + "\n");
-            }
+        /**
+         * Teams and Scores sizes does not match, if scores are bigger than 1
+         */
+        } else {
+            throw new WrongDataInputException(
+                    "Amount of scores does NOT correlate with the amount of teams!" +
+                            "\nScores : " + scores.size() +
+                            "\nTeams  : " + teams.size());
         }//endIf
         return matches;
     }//end generateSETournament
@@ -154,9 +161,9 @@ public class EliminationController implements IFEliminationController {
     /**
      * Designed to generate a random number between 0 and maximum.
      * @param input Maximum
-     * @return random Integer between0 and input
+     * @return random Integer between 0 and input (Maximum)
      */
-    public int rndNumber(int input){
+    private int rndNumber(int input){
         Random rnd = new Random();
         int i  = rnd.nextInt(input);
         return i;
@@ -168,7 +175,7 @@ public class EliminationController implements IFEliminationController {
      * @param two team two participating in the tournament
      * @return match with team one and team two battling against each other
      */
-    public Match generateMatch(Team one, Team two){
+    private Match generateMatch(Team one, Team two){
         Match match = new Match();
         match.setTeam1(one);
         match.setTeam2(two);
@@ -176,7 +183,7 @@ public class EliminationController implements IFEliminationController {
         return match;
     }
 
-    public int getMaxScore(ArrayList<Integer> scores){
+    private int getMaxScore(ArrayList<Integer> scores){
         boolean max = false;
         int i = 0;
         int maxValue = -1;
@@ -188,6 +195,12 @@ public class EliminationController implements IFEliminationController {
 
             }//endIf
             i++;
+            if(maxValue == -1){
+                throw new IndexOutOfBoundsException();
+            }
+            else if(scores.size() == i){
+                max = true;
+            }
         }//endWhile
         return maxPos;
     }//endMethod
